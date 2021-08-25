2b65341782cd964cc300624705bc5c1dd0ed2c22
JJUENGER <jason.juenger@coxautoinc.com>
2021-08-25 16:23:52 -0700

    add methods to generate diff and update network owner and group

--- plugins/modules/cloud/opennebula/one_network.py
+++ plugins/modules/cloud/opennebula/one_network.py
@@ -41,7 +41,7 @@ options:
     description:
       - ID of the cluster to create the network under
     type: int
-  owner_id:
+  user_id:
     description:
       - ID of the user which will be set as the owner of the instance
     type: int
@@ -103,12 +103,12 @@ group_name:
     type: str
     returned: when I(state=present)
     sample: one-users
-owner_id:
+user_id:
     description: network's owner id
     type: int
     returned: when I(state=present)
     sample: 143
-owner_name:
+user_name:
     description: network's owner name
     type: str
     returned: when I(state=present)
@@ -118,15 +118,16 @@ owner_name:
 
 from ansible_collections.community.general.plugins.module_utils.opennebula import OpenNebulaModule
 
+
 class NetworkModule(OpenNebulaModule):
     def __init__(self):
         argument_spec = dict(
             id=dict(type='int', required=False),
             name=dict(type='str', required=False),
-            owner=dict(type='int', required=False),
-            group=dict(type='int', required=False),
+            user_name=dict(type='str', required=False),
+            group_name=dict(type='str', required=False),
             state=dict(type='str', choices=['present', 'absent', 'query'], default='present'),
-            template=dict(type='str', required=False),
+            template=dict(type='dict', required=False),
         )
 
         mutually_exclusive = [
@@ -150,8 +151,8 @@ class NetworkModule(OpenNebulaModule):
         params = module.params
         id = params.get('id')
         name = params.get('name')
-        owner = params.get('owner')
-        group = params.get('group')
+        user_name = params.get('user_name')
+        group_name = params.get('group_name')
         desired_state = params.get('state')
         template_data = params.get('template')
 
@@ -181,7 +182,7 @@ class NetworkModule(OpenNebulaModule):
             if needs_creation:
                 self.result = self.create_network(name, template_data)
             else:
-                self.result = self.update_network(network, template_data)
+                self.result = self.update_network(network, params, template_data)
 
         self.exit()
 
@@ -222,6 +223,43 @@ class NetworkModule(OpenNebulaModule):
 
         return info
 
+    def get_user_by_name(self, user_name):
+        user_pool = self.one.userpool.info()
+
+        for user in user_pool.USER:
+            if user.NAME == user_name:
+                return user
+
+        self.fail(msg="No user with name '%s' found" % user_name)
+
+    def get_group_by_name(self, group_name):
+        group_pool = self.one.grouppool.info()
+
+        for group in group_pool.GROUP:
+            if group.NAME == group_name:
+                return group
+
+        self.fail(msg="No group with name '%s' found" % group_name)
+
+    def get_template_diff(self, existing_config, proposed_template):
+        if proposed_template is None:
+            proposed_template = dict()
+
+        if self.requires_template_update(existing_config['template'], proposed_template):
+            return True
+
+        return False
+
+    def get_parameter_diff(self, existing_config, proposed_config):
+        diff_params = []
+        for key in proposed_config:
+            if key != 'template':
+                if key in existing_config.keys() and proposed_config[key] != None:
+                    if proposed_config[key] != existing_config[key]:
+                        diff_params.append(key)
+
+        return diff_params
+
     def create_network(self, name, network_data):
         if not self.module.check_mode:
             self.one.network.allocate("NAME = \"" + name + "\"\n" + network_data)
@@ -231,18 +269,31 @@ class NetworkModule(OpenNebulaModule):
 
         return result
 
-    def update_network(self, network, network_data):
-        if not self.module.check_mode:
-            # 0 = replace the whole template
-            self.one.network.update(network.ID, network_data, 0)
-
+    def update_network(self, network, params, template):
         result = self.get_network_info(self.get_network_by_id(network.ID))
-        if self.module.check_mode:
-            # Unfortunately it is not easy to detect if the template would have changed, therefore always report a change here.
+        result['changed'] = False
+
+        if self.get_template_diff(result, template):
             result['changed'] = True
-        else:
-            # if the previous parsed template data is not equal to the updated one, this has changed
-            result['changed'] = network.TEMPLATE != result['template']
+            if not self.module.check_mode:
+                # 0 = replace the whole template
+                self.one.vn.update(network.ID, template, 0)
+
+        param_diffs = self.get_parameter_diff(result, params)
+        if param_diffs:
+            result['changed'] = True
+            if 'user_name' in param_diffs and 'group_name' in param_diffs:
+                user = self.get_user_by_name(params['user_name'])
+                group = self.get_group_by_name(params['group_name'])
+                self.one.vn.chown(network.ID, user.ID, group.ID)
+            elif 'user_name' in param_diffs:
+                user = self.get_user_by_name(params['user_name'])
+                self.one.vn.chown(network.ID, user.ID, -1)
+            elif 'group_name' in param_diffs:
+                group = self.get_group_by_name(params['group_name'])
+                self.one.vn.chown(network.ID, -1, group.ID)
+            else:
+                raise ValueError('Parameter diffs %s' % param_diffs)
 
         return result
 
